b52e7b41c46453dadafd13c3675bbb462aac1dce
GH10PAVITHRA <pavithraadvikh17@gmail.com>
2022-02-15 20:47:51 +0530

    3 files modified, 7 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,17 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule ,
+  RouterModule.forRoot([
+    {path :"" ,component : ProductListComponent}
+  ])
+  ],
+  declarations: [ AppComponent, HelloComponent ,TopBarComponent,ProductListComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { products } from '../product';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+})
+export class ProductListComponent implements OnInit {
+  product = products;
+  constructor() {}
+
+  ngOnInit() {}
+}
